81074fbd8bbad6dd57acae2c45bbc54001e80c01
Ignacio Pellejero <126637114+IgnacioPellejero@users.noreply.github.com>
2023-09-06 15:54:41 -0300

    rick

--- codigo.js
+++ codigo.js
@@ -16,25 +16,64 @@ const sr = ScrollReveal({
 
 sr.reveal(`.home_data`, { delay: 200 })
 
-sr.reveal(`.home_img`, { origin: "top" }, { delay: 200 },)
+sr.reveal(`.home_img`, { origin: "top" }, { delay: 200 },);
 
-const ubicaciones = async ()=>{
-const respuesta = await fetch("https://rickandmortyapi.com/api/location")
-const datos = await respuesta.json();
+const ubicaciones = async () => {
+    const respuesta = await fetch("https://rickandmortyapi.com/api/location");
+    const datos = await respuesta.json();
 
-datos.forEach(()=> {
-    const articulo = document.createRange().createContextualFragment(
-` <div class="home_card">
-        <h2>${nombre.name}</h2>
-        <p>${tipo.type}</p>
-        <p>${dimen.dimension}</p>
-        <p>${resid.residents}</p>
-    </div>
-`
-    )
-    const main = document.querySelector(".home_card");
-    main.append(articulo)
-})
+    // Debes recorrer la matriz 'results' dentro de 'datos'
+    datos.results.forEach((ubicacion) => {
+        const nombre = ubicacion.name;
+        const tipo = ubicacion.type;
+        const dimen = ubicacion.dimension;
+
+
+        // Debes crear un nuevo elemento div para cada ubicación
+        const articulo = document.createElement("div");
+        articulo.classList.add("card");
+        articulo.innerHTML = `
+            <h2>${nombre}</h2>
+            <p>${tipo}</p>
+            <p>${dimen}</p>
+
+        `;
+
+        const main = document.querySelector(".home_dat"); // Usar la clase correcta
+        main.appendChild(articulo); // Usar appendChild para agregar elementos al contenedor
+    });
 };
-ubicaciones()
 
+ubicaciones();
+
+
+const personajes = async () => {
+    const respuesta = await fetch("https://rickandmortyapi.com/api/character");
+    const datos = await respuesta.json();
+
+    // Debes recorrer la matriz 'results' dentro de 'datos'
+    datos.results.forEach((personajes) => {
+        const nombre = personajes.name;
+        const img = personajes.image;
+        const estado = personajes.status;
+        const especie = personajes.species;
+        const genero = personajes.gender;
+
+
+        // Debes crear un nuevo elemento div para cada ubicación
+        const articulo = document.createElement("div");
+        articulo.classList.add("card");
+        articulo.innerHTML = `
+            <h2>${nombre}</h2>
+            <img src="${img}" alt="">
+            <p>${estado}</p>
+            <p>${especie}</p>
+            <p>${genero}</p>
+        `;
+
+        const main = document.querySelector(".home_pers"); // Usar la clase correcta
+        main.appendChild(articulo); // Usar appendChild para agregar elementos al contenedor
+    });
+};
+
+personajes();
